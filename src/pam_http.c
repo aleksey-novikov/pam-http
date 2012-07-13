8fa37fc6b22547451604ac54e5b88477efd3036c
Luka Čehovin <luka.cehovin@gmail.com>
2012-07-13 10:49:16 +0200

    Adding optional API key argument. Improving HTTPS CA handling.

--- src/pam_http.c
+++ src/pam_http.c
@@ -17,9 +17,26 @@ PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const ch
 }
 
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	printf("Acct mgmt\n");
+#ifdef DEBUG
+	fprintf(stderr, "Acct mgmt\n");
+#endif
 	return PAM_SUCCESS;
 }
+
+/*size_t _read_headers( void *ptr, size_t size, size_t nmemb, void *userdata) {
+
+	char* tmp;
+	int len = size * nmemb + 1;
+	
+	tmp = malloc(len);
+
+	memcpy(tmp, ptr, len-1);
+	tmp[len-1] = '\0';
+	
+	printf("%s\n", tmp);
+
+	return size * nmemb;
+}*/
 
 /*
  * Makes getting arguments easier. Accepted arguments are of the form: name=value
@@ -29,7 +46,7 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
  * @param argv- arguments
  * @return Pointer to value or NULL
  */
-static const char* getArg(const char* pName, int argc, const char** argv) {
+static const char* _get_argument(const char* pName, int argc, const char** argv) {
 	int len = strlen(pName);
 	int i;
 
@@ -55,13 +72,16 @@ static int writeFn(void* buf, size_t len, size_t size, void* userdata) {
 	return len * size;
 }
 
-static int getUrl(const char* pUrl, const char* pUsername, const char* pPassword, const char* pCaFile) {
-	printf("Start stuff\n");
+static int perform_authentication(const char* pUrl, const char* pUsername, const char* pPassword, const char* pCaFile, const char* pKey) {
+//	printf("Start stuff\n");
 
 	CURL* pCurl = curl_easy_init();
 	int res = -1;
-
+	long http_code = 0;
+	struct curl_slist *headers=NULL; // init to NULL is important 
 	char* pUserPass;
+	char* pApiKey = NULL;
+	int apilen = 0;
 	int len = strlen(pUsername) + strlen(pPassword) + 2; // : separator & trailing null
 
 	if (!pCurl) {
@@ -77,23 +97,62 @@ static int getUrl(const char* pUrl, const char* pUsername, const char* pPassword
 	curl_easy_setopt(pCurl, CURLOPT_USERPWD, pUserPass);
 	curl_easy_setopt(pCurl, CURLOPT_NOPROGRESS, 1); // we don't care about progress
 	curl_easy_setopt(pCurl, CURLOPT_FAILONERROR, 1);
+//	curl_easy_setopt(pCurl, CURLOPT_HEADERFUNCTION, _read_headers);
+//	curl_easy_setopt(pCurl,	CURLOPT_WRITEHEADER, stderr);
 	// we don't want to leave our user waiting at the login prompt forever
-	curl_easy_setopt(pCurl, CURLOPT_TIMEOUT, 1);
+	curl_easy_setopt(pCurl, CURLOPT_TIMEOUT, 10);
 
+
+	if (pKey) {
+		apilen = 11 + strlen(pKey) + 2;
+		pApiKey = malloc(apilen);
+
+		sprintf(pApiKey, "X-Api-Key: %s\r\n", pKey);
+    		headers = curl_slist_append(headers, pApiKey);
+    		//curl_slist_append(headers, NULL);
+		curl_easy_setopt(pCurl, CURLOPT_HTTPHEADER, headers);
+
+	}
 	// SSL needs 16k of random stuff. We'll give it some space in RAM.
-	curl_easy_setopt(pCurl, CURLOPT_RANDOM_FILE, "/dev/urandom");
-	curl_easy_setopt(pCurl, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_easy_setopt(pCurl, CURLOPT_SSL_VERIFYHOST, 2);
-	curl_easy_setopt(pCurl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
+
+	if (strlen(pUrl) > 5 && strncmp(pUrl, "https", 5) == 0) {
+
+		curl_easy_setopt(pCurl, CURLOPT_RANDOM_FILE, "/dev/urandom");
+		curl_easy_setopt(pCurl, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_easy_setopt(pCurl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
+
+		if (pCaFile) {
+			curl_easy_setopt(pCurl, CURLOPT_SSL_VERIFYHOST, 2);
+			curl_easy_setopt(pCurl, CURLOPT_CAINFO, pCaFile);
+		} else {
+			curl_easy_setopt(pCurl, CURLOPT_SSL_VERIFYHOST, 0);
+		}
+
+	}
 
 	// synchronous, but we don't really care
 	res = curl_easy_perform(pCurl);
 
 	memset(pUserPass, '\0', len);
 	free(pUserPass);
+
+	if (pKey) {
+		memset(pApiKey, '\0', apilen);
+		free(pApiKey);
+		curl_slist_free_all(headers);
+	}
+
+	if (res) {
+		curl_easy_getinfo (pCurl, CURLINFO_RESPONSE_CODE, &http_code);
+		if (http_code != 200)
+			res = 22;
+	}
+
 	curl_easy_cleanup(pCurl);
 
-	printf("Res: %d\n", res);
+#ifdef DEBUG
+	fprintf(stderr, "Res: %d\n", res);
+#endif
 
 	return res;
 }
@@ -105,6 +164,7 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t* pamh, int flags, int argc, cons
 	const char* pUsername = NULL;
 	const char* pUrl = NULL;
 	const char* pCaFile = NULL;
+	const char* pKey = NULL;
 
 	struct pam_message msg;
 	struct pam_conv* pItem;
@@ -112,30 +172,32 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t* pamh, int flags, int argc, cons
 	const struct pam_message* pMsg = &msg;
 
 	msg.msg_style = PAM_PROMPT_ECHO_OFF;
-	msg.msg = "Cool buddy: ";
+	msg.msg = "Password: ";
 	
-	printf("I got called\n");
-
 	if (pam_get_user(pamh, &pUsername, NULL) != PAM_SUCCESS) {
 		return PAM_AUTH_ERR;
 	}
 
-	pUrl = getArg("url", argc, argv);
+	pUrl = _get_argument("url", argc, argv);
 	if (!pUrl) {
 		return PAM_AUTH_ERR;
 	}
 
-	pCaFile = getArg("cafile", argc, argv);
+	pCaFile = _get_argument("cafile", argc, argv);
 	if (pam_get_item(pamh, PAM_CONV, (const void**)&pItem) != PAM_SUCCESS || !pItem) {
+#ifdef DEBUG
 		fprintf(stderr, "Couldn't get pam_conv\n");
+#endif
 		return PAM_AUTH_ERR;
 	}
+
+	pKey = _get_argument("key", argc, argv);
 
 	pItem->conv(1, &pMsg, &pResp, pItem->appdata_ptr);
 
 	ret = PAM_SUCCESS;
 
-	if (getUrl(pUrl, pUsername, pResp[0].resp, pCaFile) != 0) {
+	if (perform_authentication(pUrl, pUsername, pResp[0].resp, pCaFile, pKey) != 0) {
 		ret = PAM_AUTH_ERR;
 	}
 	
