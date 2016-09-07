960058ac1e56d36c91e132ccf106c886950ca81b
Luka Čehovin <luka.cehovin@gmail.com>
2016-09-07 15:19:13 +0100

    Adding caching functionality (useful when securing HTTP requests).

--- pam_http.c
+++ pam_http.c
@@ -1,9 +1,18 @@
+#define _XOPEN_SOURCE    
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <security/pam_modules.h>
 #include <syslog.h>
+#include <time.h>
 #include <curl/curl.h>
+
+#define DEFAULT_SALT "$6$2e1d104a249e35a966fa44b587108a172b7a90d5ce2b276971a2181187225cb8"
 
 /* expected hook */
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
@@ -58,6 +67,111 @@ static const char* _get_argument(const char* pName, int argc, const char** argv)
  */
 static int writeFn(void* buf, size_t len, size_t size, void* userdata) {
 	return len * size;
+}
+
+int lookup_cache(const char* pCacheDir, const char* pUsername, const char* pPassword, const char* pSalt, int timeout) {
+
+    struct stat attrib;
+    time_t now_time;
+    int len_dir, len_file, len_payload, i;
+    char* path;
+    char* password_hash;
+    char* username_hash;
+
+    password_hash = strdup(crypt(pPassword, pSalt));
+    username_hash = strdup(crypt(pUsername, pSalt));
+
+    len_dir = strlen(pCacheDir);
+    len_file = strlen(username_hash);
+    len_payload = strlen(password_hash);
+
+    path = (char *) malloc(sizeof(char) * (len_dir + len_file + 1));
+
+    if (pCacheDir[len_dir-1] == '/') 
+        sprintf(path, "%s%s", pCacheDir, username_hash);
+    else
+        sprintf(path, "%s/%s", pCacheDir, username_hash);
+
+    time (&now_time);
+    if (stat(path, &attrib) == 0) {
+
+        double age = difftime(now_time, attrib.st_ctime);
+        if (age < timeout) {
+
+            FILE* fd = fopen(path, "r");
+
+            if (fd) {
+                int match = 1;
+
+                for (i = 0; i < len_payload; i++) {
+                    int c = getc(fd);
+
+                    if (c == EOF || password_hash[i] != (char) c) {
+                        match = 0;
+                        break;
+                    }
+
+                }
+
+                fclose(fd);
+                free(password_hash);
+                free(username_hash);
+                free(path);
+
+                syslog(LOG_DEBUG, "Cached password for user %s %s.", pUsername, match ? "matches" : "does not match");
+
+                return match ? 0 : -1;
+            }
+        } else {
+            syslog(LOG_DEBUG, "Cache for user %s exists but is stale (%.0f seconds).", pUsername, age);
+        }
+    } else {
+            syslog(LOG_DEBUG, "Cache for user %s does not exist.", pUsername);
+    }
+
+    free(password_hash);
+    free(username_hash);
+    free(path);
+    return -1;
+}
+
+int store_cache(const char* pCacheDir, const char* pUsername, const char* pPassword, const char* pSalt) {
+
+    int len_dir, len_file;
+    char* path;
+    FILE* fd;
+    char* password_hash;
+    char* username_hash;
+
+    password_hash = strdup(crypt(pPassword, pSalt));
+    username_hash = strdup(crypt(pUsername, pSalt));
+
+    len_dir = strlen(pCacheDir);
+    len_file = strlen(username_hash);
+
+    path = (char *) malloc(sizeof(char) * (len_dir + len_file + 1));
+
+    if (pCacheDir[len_dir-1] == '/') 
+        sprintf(path, "%s%s", pCacheDir, username_hash);
+    else
+        sprintf(path, "%s/%s", pCacheDir, username_hash);
+
+    fd = fopen(path, "w");
+    if (!fd) {
+        syslog(LOG_DEBUG, "Unable to write cache entry, directory does not exist?");
+        return -1;
+    }
+
+    fputs(password_hash, fd);
+    fclose(fd);
+
+    syslog(LOG_DEBUG, "Writing cache entry for user %s.", pUsername);
+
+    free(password_hash);
+    free(username_hash);
+    free(path);
+
+    return 0;
 }
 
 static int perform_authentication(const char* pUrl, const char* pUsername, const char* pPassword, const char* pCaFile, const char* pKey, const char* pHost, const int timeout) {
@@ -164,13 +278,17 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t* pamh, int flags, int argc, cons
 	const char* pKey = NULL;
 	const char* pTimeout = NULL;
 	const char* pHost = NULL;
+	const char* pSalt = NULL;
+	const char* pCacheDir = NULL;
+	const char* pCacheTimeout = NULL;
 
 	struct pam_message msg;
 	struct pam_conv* pItem;
 	struct pam_response* pResp;
 	const struct pam_message* pMsg = &msg;
 
-	int timeout = 10;
+	int connection_timeout = 10;
+	int cache_timeout = 60 * 10; // 10 minutes
 
     openlog("pam_http", LOG_ODELAY, LOG_AUTH);
 
@@ -219,14 +337,45 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t* pamh, int flags, int argc, cons
 		pTimeout = getenv("PAM_HTTP_TIMEOUT");
 
 	if (pTimeout) {
-		timeout = atoi(pTimeout);
-		if (timeout < 1) timeout = 1;
+		connection_timeout = atoi(pTimeout);
+		if (connection_timeout < 1) connection_timeout = 1;
 	}
 
 	pKey = _get_argument("key", argc, argv);
 
 	if (!pKey)
 		pKey = getenv("PAM_HTTP_KEY");
+
+
+	pCacheDir = _get_argument("cache_dir", argc, argv);
+
+	if (!pCacheDir)
+		pCacheDir = getenv("PAM_HTTP_CACHE_DIR");
+
+	if (!pCacheDir) {
+        syslog(LOG_WARNING, "Cache dir not provided via cache_dir parameter or PAM_HTTP_CACHE_DIR.");
+	}
+
+    pSalt = _get_argument("cache_salt", argc, argv);
+
+	if (!pSalt)
+		pSalt = getenv("PAM_HTTP_SECRET");
+
+	if (!pSalt) {
+        syslog(LOG_WARNING, "Cache salt not provided via cache_salt parameter or PAM_HTTP_CACHE_SECRET, using default.");
+		pSalt = DEFAULT_SALT;
+	}
+
+	pTimeout = _get_argument("cache_timeout", argc, argv);
+
+	if (!pCacheTimeout)
+		pCacheTimeout = getenv("PAM_HTTP_TIMEOUT");
+
+	if (pCacheTimeout) {
+		cache_timeout = atoi(pCacheTimeout);
+		if (cache_timeout < 1) cache_timeout = 1;
+	}
+
 
 	pItem->conv(1, &pMsg, &pResp, pItem->appdata_ptr);
 
@@ -237,9 +386,16 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t* pamh, int flags, int argc, cons
 
 	ret = PAM_SUCCESS;
 
-	if (perform_authentication(pUrl, pUsername, pResp[0].resp, pCaFile, pKey, pHost, timeout) != 0) {
+    if (pCacheDir && lookup_cache(pCacheDir, pUsername, pResp[0].resp, pSalt, cache_timeout) == 0) {
+        return ret;
+    }
+
+	if (perform_authentication(pUrl, pUsername, pResp[0].resp, pCaFile, pKey, pHost, connection_timeout) != 0) {
 		ret = PAM_AUTH_ERR;
-	}
+	} else {
+        if (pCacheDir)
+            store_cache(pCacheDir, pUsername, pResp[0].resp, pSalt);
+    }
 
 	memset(pResp[0].resp, 0, strlen(pResp[0].resp));
 	free(pResp);
